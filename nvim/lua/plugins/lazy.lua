176d12c9e0819d0143c3d73f17545f6c4f422a2b
Shaunak Pal <illusion@illusion.local>
2025-11-23 21:11:48 +0530

    feat: Enable Nvim Supermaven; update Aerospace Cmd keybindings

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -28,29 +28,29 @@ else
 end
 
 require('lazy').setup({
-  -- {
-  --   "supermaven-inc/supermaven-nvim",
-  --   config = function()
-  --     require("supermaven-nvim").setup({
-  --       keymaps = {
-  --         accept_suggestion = "<M-tab>",
-  --         clear_suggestion = "<M-c>",
-  --         accept_word = "<C-j>",
-  --       },
-  --       ignore_filetypes = { env = true }, -- or { "cpp", }
-  --       color = {
-  --         suggestion_color = "#ff0000",
-  --         cterm = 244,
-  --       },
-  --       log_level = "info", -- set to "off" to disable logging completely
-  --       disable_inline_completion = false, -- disables inline completion for use with cmp
-  --       disable_keymaps = false, -- disables built in keymaps for more manual control
-  --       condition = function()
-  --         return false
-  --       end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
-  --     })
-  --   end,
-  -- },
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({
+        keymaps = {
+          accept_suggestion = "<M-tab>",
+          clear_suggestion = "<M-c>",
+          accept_word = "<C-j>",
+        },
+        ignore_filetypes = { env = true }, -- or { "cpp", }
+        color = {
+          suggestion_color = "#ff0000",
+          cterm = 244,
+        },
+        log_level = "info", -- set to "off" to disable logging completely
+        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_keymaps = false, -- disables built in keymaps for more manual control
+        condition = function()
+          return false
+        end -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      })
+    end,
+  },
   'onsails/lspkind.nvim',
   'folke/zen-mode.nvim',
   {
@@ -63,6 +63,7 @@ require('lazy').setup({
           enabled = false,
         },
       },
+      nes = { enabled = false },
     },
     keys = {
       {
